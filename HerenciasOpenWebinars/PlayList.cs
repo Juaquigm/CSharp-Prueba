298a1bff94fd9931d9da84f418da2e6083ff1ee3
Juaquigm <jmorongo@gmail.com>
2020-03-17 20:52:27 +0100

    Introduccion de particularidades de C#

---
+++ HerenciasOpenWebinars/PlayList.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace HerenciasOpenWebinars
+{
+    public class PlayList<T> // Va a afectar generico
+    {
+        T[] medialist = new T[] { };
+
+        public void Add(T media)
+        {
+
+        }
+
+        public void PlayAll()
+        {
+            foreach(T media in medialist)
+            {
+                Console.WriteLine(media);
+            }
+        }
+    }
+}
--- HerenciasOpenWebinars/Program.cs
+++ HerenciasOpenWebinars/Program.cs
@@ -17,11 +17,25 @@ namespace HerenciasOpenWebinars
             song2.Seconds = 200;
             song2.Position = new ShelvePosition(3, 4);
 
+            PlayList<Song> playListSongs = new PlayList<Song>();
+
+            playListSongs.Add(song);
+            playListSongs.Add(song2);
+
+            WriteLineSpecial(song, "Start with", "enjoy");
+
+            Tuple<int, string, Song> tuple = new Tuple<int, string, Song>(0, "Hola", song);
+
             // Aqui compara solo los titulos 
             Console.WriteLine(song.Equals(song2));
             
             Console.WriteLine(song2.ToString());
             Console.ReadKey(); // Pause cuando demos enter pues terminaria en nuestro casp
+        }
+
+        static void WriteLineSpecial<T>(T data, string prefix, string suffix)
+        {
+            Console.WriteLine($"{prefix} {data} {suffix}");
         }
     }
 }
