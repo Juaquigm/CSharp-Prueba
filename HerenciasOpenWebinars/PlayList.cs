e4f150f8424ab6ad3eb3e92360ad9e03daa9545a
Juaquigm <jmorongo@gmail.com>
2020-03-17 21:31:50 +0100

    Colecciones: Lista, pila y cola

--- HerenciasOpenWebinars/PlayList.cs
+++ HerenciasOpenWebinars/PlayList.cs
@@ -1,20 +1,34 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
 namespace HerenciasOpenWebinars
 {
     public class PlayList<T> // Va a afectar generico
     {
-        T[] medialist = new T[] { };
+        ArrayList list = new ArrayList();
+        List<T> mediaList = new List<T>();
 
         public void Add(T media)
         {
-
+            list.Add(media);
         }
 
         public void PlayAll()
         {
-            foreach(T media in medialist)
+            foreach(T media in list)
             {
                 Console.WriteLine(media);
+            }
+        }
+
+        public void Preview()
+        {
+            if (list.Count > 0)
+            {
+                // Esto nos hace con el ArrayList T media = (T)list[0];
+                // Con las listas no nos hace falta
+                Console.WriteLine(mediaList[0]);
             }
         }
     }
--- HerenciasOpenWebinars/Program.cs
+++ HerenciasOpenWebinars/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HerenciasOpenWebinars
 {
@@ -26,6 +27,14 @@ namespace HerenciasOpenWebinars
 
             Tuple<int, string, Song> tuple = new Tuple<int, string, Song>(0, "Hola", song);
 
+           
+            ListExplanation();
+            StackExplanation();
+            QueueExplanation();
+
+
+
+
             // Aqui compara solo los titulos 
             Console.WriteLine(song.Equals(song2));
             
@@ -33,9 +42,32 @@ namespace HerenciasOpenWebinars
             Console.ReadKey(); // Pause cuando demos enter pues terminaria en nuestro casp
         }
 
+        private static void ListExplanation()
+        {
+            List<int> numbers = new List<int>();
+            numbers.Add(0);
+            //numbers.Count
+            //Dist
+        }
+        private static void StackExplanation()
+        {
+            /*
+            Stack<Song> salesSongs = new Stack<Song>();
+            salesSongs.Push(new Song());
+            Song song = salesSongs.Pop();
+            */
+        }
+        private static void QueueExplanation()
+        {
+            Queue<int> queueNumbers = new Queue<int>();
+            queueNumbers.Enqueue(1);
+            queueNumbers.Dequeue();
+        }
         static void WriteLineSpecial<T>(T data, string prefix, string suffix)
         {
             Console.WriteLine($"{prefix} {data} {suffix}");
         }
+
+       
     }
 }
